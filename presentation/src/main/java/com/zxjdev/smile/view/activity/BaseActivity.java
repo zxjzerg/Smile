de59a6bda340b0d791396bb56c7478711813e5d3
Andrew Zhao <zxjzerg@outlook.com>
2016-07-04 16:25:19 +0800

    调整项目结构; 添加data和domain两个module;

---
+++ presentation/src/main/java/com/zxjdev/smile/view/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.zxjdev.smile.view.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Andrew on 7/4/16.
+ */
+public abstract class BaseActivity extends Activity {
+
+}
---
+++ presentation/src/main/java/com/zxjdev/smile/view/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.zxjdev.smile.view.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Andrew on 7/4/16.
+ */
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
