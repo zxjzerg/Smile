83d3a8d63d96796dba9bea395e0a0adb4c8d70e3
Andrew Zhao <zxjzerg@outlook.com>
2016-07-06 15:22:24 +0800

    添加登录和注册的界面;

---
+++ presentation/src/main/java/com/zxjdev/smile/model/UserModel.java
@@ -0,0 +1,8 @@
+package com.zxjdev.smile.model;
+
+/**
+ * Created by Andrew on 7/6/16.
+ */
+public class UserModel {
+
+}
--- presentation/src/main/java/com/zxjdev/smile/view/activity/BaseActivity.java
+++ presentation/src/main/java/com/zxjdev/smile/view/activity/BaseActivity.java
@@ -1,10 +1,19 @@
 package com.zxjdev.smile.view.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 
 /**
+ * 所有Activity的基类
  * Created by Andrew on 7/4/16.
  */
 public abstract class BaseActivity extends Activity {
 
+    protected Context mContext = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
