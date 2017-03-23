cda357c284414a38203cd00b3050476616d1bee5
Andrew Zhao <zxjzerg@outlook.com>
2017-03-23 10:21:27 +0800

    Remove moment list adapter provider

--- app-presentation/src/main/java/com/zxjdev/smile/presentation/moment/list/di/MomentListFragmentModule.java
+++ app-presentation/src/main/java/com/zxjdev/smile/presentation/moment/list/di/MomentListFragmentModule.java
@@ -1,13 +1,8 @@
 package com.zxjdev.smile.presentation.moment.list.di;
 
-import com.zxjdev.smile.presentation.application.di.DiConstant;
 import com.zxjdev.smile.presentation.application.di.scope.FragmentScope;
-import com.zxjdev.smile.presentation.application.util.image.ImageLoader;
 import com.zxjdev.smile.presentation.moment.list.MomentListContract;
 import com.zxjdev.smile.presentation.moment.list.MomentListPresenter;
-import com.zxjdev.smile.presentation.moment.list.adapter.MomentAdapter;
-
-import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
@@ -19,13 +14,6 @@ public class MomentListFragmentModule {
 
     public MomentListFragmentModule(MomentListContract.View view) {
         this.view = view;
-    }
-
-    @Provides
-    @FragmentScope
-    MomentAdapter provideMomentAdapter(@Named(
-        DiConstant.FRAGMENT_IMAGE_LOADER) ImageLoader imageLoader) {
-        return new MomentAdapter(imageLoader);
     }
 
     @Provides
